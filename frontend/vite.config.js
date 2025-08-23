344a0861c8a5e1a7b47eec1832763fce4d0997de
Adryan-Francisco <adryanfrancisco62@gmail.com>
2025-08-23 12:47:21 -0300

    Corrige dependências faltando

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/controle-imoveis-react/', // <-- ADICIONE ESTA LINHA
+  base: '/controle-imoveis-react/',
 })
